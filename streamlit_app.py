17a01a133b3d36cad1cb87bf709b05668e36634f
CSimonini <73425362+CSimonini@users.noreply.github.com>
2022-12-16 09:50:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,22 +31,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-""" 
-Before
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# GET API Request
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the JSON version of the response and normilize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-"""
-
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
