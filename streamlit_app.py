320dafb90f992aa87d679031504dbf8d132d7411
CSimonini <73425362+CSimonini@users.noreply.github.com>
2022-12-16 09:44:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-""" Before
+""" 
+Before
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
